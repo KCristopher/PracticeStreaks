c7fce961c704b08a4ddf3417e5cff38ca29e6b3e
KCristopher <avramsrant@gmail.com>
2024-10-20 14:24:11 +0200

    Removing calc_flexible_interval_streak_value functions as handling flexible interval will be handled by using additional argument in calc_fixed_interval_streak_value function.

--- calculating_streaks_draft.py
+++ calculating_streaks_draft.py
@@ -21,40 +21,37 @@ for p, n in zip(answers_only, answers_only[ 1 : ] ) :
     #print(p, n, "Current streak value:", streak_value)
 
 
-def calc_fixed_interval_streak_value(usr_answers_by_dates, step = 1 ) :
+def calc_fixed_interval_streak_value(usr_answers_by_dates, step = 1, flexible_interval = True ) :
     
     answers_only = [ v for v in usr_answers_by_dates.values() ]
     streak_value = 0
 
+    if ( len(answers_only) == 1 ) and (step == 1) and (answers_only[0] == 'yes') :
+        return 1
+    
+    elif len(answers_only) == 0 :
+        return 0
+
+        
+
     for p, n in zip(answers_only[ 0 : : step ], answers_only[ 1 :  ] [ 0 : : step ] ) :
+
         if p == n :
             streak_value += 1
         else :
-            if n == 'yes' :
+            if ( flexible_interval ) and ( [ p, n ].count('yes') >= step ) :
+                streak_value += 1
+            elif n == 'yes' :
                 streak_value = 1
             else :
-                streak_value = 0
+                streak_value = 0   
 
     return streak_value
 
 
 #print(calc_fixed_interval_streak_value(example_answers_by_dates, 2))
 
-def calc_flexible_interval_streak_value(usr_answers_by_dates, step = 1 ) :
-    
-    answers_only = [ v for v in usr_answers_by_dates.values() ]
-    streak_value = 0
 
-    for p, n in zip(answers_only[ 0 : : step ], answers_only[ 1 :  ] [ 0 : : step ] ) :
-        if [ p, n ].count('yes') >= 1 :
-            streak_value += 1
-        else :
-            if n == 'yes' :
-                streak_value = 1
-            else :
-                streak_value = 0
-    
-    return streak_value
 
 
 print(calc_flexible_interval_streak_value(example_answers_by_dates, 7))
